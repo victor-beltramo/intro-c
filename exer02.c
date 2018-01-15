464c4abf62bb49c669aaa5b6e4153da374f81d3d
Victor <foxbeltramo@hotmail.com>
2018-01-15 16:10:00 -0200

    Init code

---
+++ exer02.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main (){
+
+	int numero =0;
+	int soma =0;
+	int resultado =0;
+	scanf ("%d", &numero);
+
+
+
+	while(soma != numero){
+		soma++;
+		resultado = soma+resultado;
+		
+
+	}
+
+	printf ("A soma é %d\n", resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
+
---
+++ exer1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+	int numero, resultado;
+	numero = -1;
+	resultado = numero*numero;
+	
+		while (numero != 0){
+			scanf ("%d", &numero);
+			resultado = numero*numero;
+			printf ("O quadrado do numero %d eh %d\n ", numero, resultado);
+	
+	
+	}
+
+
+
+return 0;
+}
+
+
+
